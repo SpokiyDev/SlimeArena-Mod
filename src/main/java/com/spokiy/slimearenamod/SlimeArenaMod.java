b3ee4b9a1ee09f4d4f5a953395b35e6943e93664
Pavlo <99823359+Pashan4ik@users.noreply.github.com>
2026-03-21 14:25:00 +0100

    Initial commit

---
+++ src/main/java/com/spokiy/slimearenamod/SlimeArenaMod.java
@@ -0,0 +1,15 @@
+package com.spokiy.slimearenamod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SlimeArenaMod implements ModInitializer {
+	public static final String MOD_ID = "slimearenamod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/spokiy/slimearenamod/SlimeArenaModClient.java
@@ -0,0 +1,13 @@
+package com.spokiy.slimearenamod;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SlimeArenaModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
